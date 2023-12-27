c9b750e92a918e8394b1d65816550fdf4351be3c
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2023-12-27 13:12:17 -0400

    Añadiendo endpoint put. GC

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Text, Optional
 from fastapi import FastAPI, HTTPException , status
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from uuid import uuid4 as UIDI
 
@@ -25,24 +26,24 @@ class publicacionID(BaseModel):
 lits_posts = []
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hola Mundo"}
 
 @app.get("/posts")
-def get_post():
+async def get_post():
     if lits_posts:
         return lits_posts
     raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="No hay publicaciones que mostrar" )
 
 @app.get("/post/{id}")
-def get_post(id:str):
+async def get_post(id:str):
     for post in lits_posts:
         if post.get("id") == id:
             return post
     raise HTTPException (status_code=status.HTTP_404_NOT_FOUND)
 
 @app.post("/posts")
-def save_post(post_id: publicacionID):
+async def save_post(post_id: publicacionID):
     post = publicacion(**dict(post_id), id=str(UIDI()), published_at=datetime.now())
     if post:
         lits_posts.append(post.dict())
@@ -50,9 +51,17 @@ def save_post(post_id: publicacionID):
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No se ha podido crear la publicacion")
 
 @app.delete("/posts")
-def delete_posts (id:str):
+async def delete_posts (id:str):
     for post in lits_posts:
         if post.get("id") == id:
             lits_posts.remove(post)
             raise HTTPException(status_code= status.HTTP_302_FOUND, detail=f"Publicacion con ID: {id} eliminado")
+    raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"Publicacion con ID: {id} no encontrada")
+
+@app.put("/posts")
+async def add_Post(post : publicacionID, id:str):
+    for i, public  in enumerate(lits_posts):
+        if public.get("id") == id:
+            lits_posts[i] = publicacion(**dict(post), id=str(id), published_at=datetime.now())
+            return JSONResponse(content={"message": "producto actualizado correctamente"})
     raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"Publicacion con ID: {id} no encontrada")
